5474c86298b2cc9f76a5da1e9350d56b9b06d9d2
baylend123 <75502053+baylend123@users.noreply.github.com>
2021-04-26 10:14:14 -0500

    “model edits”

--- project-starter/app/models/user.py
+++ project-starter/app/models/user.py
@@ -4,80 +4,75 @@ from flask_login import UserMixin
 
 
 class User(db.Model, UserMixin):
-  __tablename__ = 'users'
+    __tablename__ = 'users'
 
-  id = db.Column(db.Integer, primary_key = True)
-  username = db.Column(db.String(40), nullable = False, unique = True)
-  email = db.Column(db.String(255), nullable = False, unique = True)
-  hashed_password = db.Column(db.String(255), nullable = False)
-  userCoins = db.relationship("Watchlist", backref="user")
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    hashed_password = db.Column(db.String(255), nullable=False)
+    userCoins = db.relationship("Watchlist", backref="user")
 
+    @property
+    def password(self):
+        return self.hashed_password
 
-  @property
-  def password(self):
-    return self.hashed_password
+    @password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
 
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
-  @password.setter
-  def password(self, password):
-    self.hashed_password = generate_password_hash(password)
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email
+        }
 
-
-  def check_password(self, password):
-    return check_password_hash(self.password, password)
-
-
-  def to_dict(self):
-    return {
-      "id": self.id,
-      "username": self.username,
-      "email": self.email
-    }
 
 class Watchlist(db.Model):
-  __tablename__ = 'watchlists'
-  __table_args__ = (
-    db.UniqueConstraint('userId', 'coinId', name="unique_coin"),
+    __tablename__ = 'watchlists'
+    __table_args__ = (
+        db.UniqueConstraint('userId', 'coinId', name="unique_coin"),
     )
 
-  id = db.Column(db.Integer, primary_key=True)
-  userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-  coinId = db.Column(db.Integer, nullable=False)
-  user = db.relationship("User", backref="userCoins")
-  coinId = db.Column(db.Integer, db.ForeignKey('coins.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    coinId = db.Column(db.Integer, db.ForeignKey('coins.id'), nullable=False)
+    user = db.relationship("User", backref="userCoins")
 
 
 class Portfolio(db.Model):
-  __tablename__ = "portfolios"
-  __table_args__ = (
-      db.UniqueConstraint('userId', 'coinId', name='own_coin_once'),
+    __tablename__ = "portfolios"
+    __table_args__ = (
+        db.UniqueConstraint('userId', 'coinId', name='own_coin_once'),
     )
 
-  id = db.Column(db.Integer, primary_key=True)
-  userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-  coinId = db.Column(db.Integer, db.ForeignKey('coins.id'), nullable=False)
-  quantity = db.Column(db.Float, nullable=False)
-  averagePrice = db.Column(db.Float, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    coinId = db.Column(db.Integer, db.ForeignKey('coins.id'), nullable=False)
+    quantity = db.Column(db.Float, nullable=False)
+    averagePrice = db.Column(db.Float, nullable=False)
 
 
 class Coin(db.Model):
-  __tablename__ = "coins"
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(), nullable=False)
-  ticker = db.Column(db.String(), nullable=False)
+    __tablename__ = "coins"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    ticker = db.Column(db.String(), nullable=False)
+
 
 class Trade(db.Model):
-  __tablename__ = 'trades'
+    __tablename__ = 'trades'
 
-  id = db.Column(db.Integer, primary_key=True)
-  tradePrice = db.Column(db.Float, nullable=False)
-  tradeSize = db.Column(db.Float, nullable=False)
-  buyOrSell = db.Column(db.Boolean, nullable=False)
-  dateOfTrade = db.Column(db.DateTime, nullable=False)
-  userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-  coinId = db.Column(db.Integer, db.ForeignKey('coins.id'), nullable=False)
-
-
+    id = db.Column(db.Integer, primary_key=True)
+    tradePrice = db.Column(db.Float, nullable=False)
+    tradeSize = db.Column(db.Float, nullable=False)
+    buyOrSell = db.Column(db.Boolean, nullable=False)
+    dateOfTrade = db.Column(db.DateTime, nullable=False)
+    userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    coinId = db.Column(db.Integer, db.ForeignKey('coins.id'), nullable=False)
 
 
 # watchlists
