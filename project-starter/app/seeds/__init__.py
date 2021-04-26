08a7bf08a95c1534af105f52914b0da66c90a72d
Nishi <naalnishinelson@gmail.com>
2021-04-25 18:40:48 -0700

    seeds

--- project-starter/app/seeds/__init__.py
+++ project-starter/app/seeds/__init__.py
@@ -1,5 +1,5 @@
 from flask.cli import AppGroup
-from .users import seed_users, undo_users
+from .users import seed_users, undo_users, seed_coins, seed_portfolio, undo_coins, seed_trades, seed_watchlist, undo_portfolios, undo_trades, undo_watchlists
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -9,10 +9,19 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
+    seed_coins()
+    seed_portfolio()
+    seed_trades()
+    seed_watchlist()
     # Add other seed functions here
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_coins()
+    undo_portfolios()
+    undo_watchlists()
+    undo_trades()
+
     # Add other undo functions here
--- project-starter/app/seeds/users.py
+++ project-starter/app/seeds/users.py
@@ -1,5 +1,5 @@
 # from werkzeug.security import generate_password_hash
-from app.models import db, User
+from app.models import db, User, Coin, Portfolio, Watchlist, Trade
 
 
 # Adds a demo user, you can add other users here if you want
@@ -13,10 +13,57 @@ def seed_users():
     db.session.commit()
 
 
+def seed_coins():
+    bitcoin = Coin(name='Bitcoin', ticker='BTC-USD')
+
+    db.session.add(bitcoin)
+    db.session.commit()
+
+
+def seed_portfolio():
+    demo = Portfolio(userId=1, coinId=1, quantity=5, averagePrice=50000)
+
+    db.session.add(demo)
+    db.session.commit()
+
+
+def seed_watchlist():
+    demo = Watchlist(userId=1, coinId=1)
+
+    db.session.add(demo)
+    db.session.commit()
+
+
+def seed_trades():
+    demo = Trade(tradePrice=10000, tradeSize=5, buyOrSell=true, dateOfTrade='04-25-2021', userId=1, coinId=1)
+
+
 # Uses a raw SQL query to TRUNCATE the users table.
 # SQLAlchemy doesn't have a built in function to do this
 # TRUNCATE Removes all the data from the table, and resets
 # the auto incrementing primary key
+
+
 def undo_users():
     db.session.execute('TRUNCATE users RESTART IDENTITY CASCADE;')
+    db.session.commit()
+
+
+def undo_coins():
+    db.session.execute('TRUNCATE coins RESTART IDENTITY CASCADE;')
+    db.session.commit()
+
+
+def undo_portfolios():
+    db.session.execute('TRUNCATE portfolios RESTART IDENTITY CASCADE;')
+    db.session.commit()
+
+
+def undo_watchlists():
+    db.session.execute('TRUNCATE watchlists RESTART IDENTITY CASCADE;')
+    db.session.commit()
+
+
+def undo_trades():
+    db.session.execute('TRUNCATE trades RESTART IDENTITY CASCADE;')
     db.session.commit()
