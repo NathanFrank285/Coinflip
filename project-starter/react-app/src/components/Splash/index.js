23897d6a0569f6bb45e61dee863e21082cc466ec
Robert Burroughs <clifdevelops@gmail.com>
2021-04-26 14:09:48 -0600

    adding logo and some css

--- project-starter/react-app/src/components/Splash/index.js
+++ project-starter/react-app/src/components/Splash/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import LoginForm from "../auth/LoginForm"
 import SignUpForm from "../auth/SignUpForm"
+import DarkLogo from "../../images/DarkLogo.png"
 import "./splash.css"
 
 const Splash = () => {
   return (
     <div className='splash-body'>
-        <div className='splash-title'> Welcome to Coinflip. </div>
+        
+        <img className='splash-logo' src={DarkLogo} alt='logo' />
+        
+        <div className='splash-title'> Welcome to Coinflip </div>
         <div className='splash-forms'>
             <div className='splash-login'> <LoginForm /> </div>
             <div className='splash-signup'> <SignUpForm /> </div>
