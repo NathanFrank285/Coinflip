1138e96522cc438124cd1ed49d1b40c39df16969
Nishi <naalnishinelson@gmail.com>
2021-04-26 17:00:18 -0700

    searchbar updates

---
+++ project-starter/react-app/src/components/Searchbar/index.js
@@ -0,0 +1,40 @@
+import { getSearch } from '../../store/search';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import './search.css';
+
+
+const SearchBar = () => {
+    const [keyword, setKeyword] = useState("")
+    const history = useHistory()
+    const dispatch = useDispatch();
+
+    const submit = (e) => {
+      e.preventDefault();
+      dispatch(getSearch(keyword));
+      history.push("/coinDetail")
+    }
+
+
+    return (
+      <div>
+        <form
+          method="get"
+          action="/search/"
+          onSubmit={(e) => submit(e)}>
+
+          <input
+            value={keyword}
+            placeholder="Search By State"
+            onChange={(e) => setKeyword(e.target.value)}>
+
+          </input>
+
+          <button onClick={(e) => submit(e)} type="submit">Find!</button>
+        </form>
+      </div>
+    )
+  }
+
+  export default SearchBar
--- project-starter/react-app/src/store/index.js
+++ project-starter/react-app/src/store/index.js
@@ -1,10 +1,12 @@
 import {createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from 'redux-thunk';
 import session from './session'
+import search from './search'
 
 
 const rootReducer = combineReducers({
-    session
+    session,
+    search
 });
 
 let enhancer;
