e947af09e3597bc5fcf8eeff24b9d9ff931d7eec
baylend123 <75502053+baylend123@users.noreply.github.com>
2021-04-27 21:06:56 -0500

    “chart stuff”

--- project-starter/react-app/src/components/CoinDetail/index.js
+++ project-starter/react-app/src/components/CoinDetail/index.js
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getCoinDetailThunk } from '../../store/coinDetail'
 import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser'
 import './CoinDetail.css'
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
 
 const CoinDetail = () => {
@@ -12,19 +13,30 @@ const CoinDetail = () => {
     const history = useHistory();
     const { name } = useParams()
     const details = useSelector(state => state?.coinDetail?.coin)
+    const chartData = useSelector(state => state?.coinDetail?.prices)
     useEffect(() => {
         let response = dispatch(getCoinDetailThunk(name))
         //todo figure out how to redirect when there is a bad search url
-        if (!response){
+        if (!response) {
             history.push('/portfolio')
         }
     }, [])
+    console.log(chartData)
+    const formatCash = (n) => {
+        if (n < 1e3) return n;
+        if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + "K";
+        if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + "M";
+        if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + "B";
+        if (n >= 1e12) return +(n / 1e12).toFixed(2) + "T";
+    };
+
     return (
         <div className="priceDetailContainer">
             <div className="header">
 
                 <span>
                     <h1>{details?.name}</h1>
+
                     <h2>{details?.symbol.toUpperCase()}</h2>
                 </span>
                 <span>
@@ -38,14 +50,24 @@ const CoinDetail = () => {
                 </div>
             </div>
             <div className="graphDiv">
-                GRAPH PLACEHOLDER
+                <LineChart width={730} height={400} data={chartData}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+
+                    <YAxis domain={['auto', 'auto']} />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="linear" dataKey="price" stroke="#8884d8" />
+                </LineChart>
             </div>
             <div className="maketDetail">
-                <div>Current Price: {details?.market_data?.current_price.usd}</div>
-                <div>Total Supply: {details?.market_data?.circulating_supply}</div>
-                <div>Market Cap : {details?.market_data?.market_cap.usd}</div>
-                <div>24Hr Change: {details?.market_data?.price_change_24h}%</div>
-                <div>Total Volume: {details?.market_data?.total_volume.usd}</div>
+                <div>Current Price: {details?.market_data?.current_price.usd.toLocaleString("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                })}</div>
+                <div>Total Supply: {formatCash(details?.market_data?.circulating_supply)}</div>
+                <div>Market Cap : {formatCash(details?.market_data?.market_cap.usd)}</div>
+                <div>24Hr Change: {details?.market_data?.price_change_24h.toFixed(2)}%</div>
+                <div>Total Volume: {formatCash(details?.market_data?.total_volume.usd)}</div>
             </div>
             <div className='description'>
                 {ReactHtmlParser(details?.description.de)}
