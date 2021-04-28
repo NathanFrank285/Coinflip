2dfd976e828091726f8693caad986c51b4594673
NathanFrank285 <nathansfrank@yahoo.com>
2021-04-28 15:23:46 -0700

    returning user's portfolio

---
+++ project-starter/react-app/src/components/PortfolioGraph/index.js
@@ -0,0 +1,19 @@
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+} from "recharts";
+import './PortfolioGraph.css'
+
+
+export default function PortfolioGraph(){
+
+
+  return (
+    'this is my graph'
+  )
+}
