86611bc30cb051f4394f77cb5189dd1a90ce363b
baylend123 <75502053+baylend123@users.noreply.github.com>
2021-04-28 17:43:41 -0500

    “searchlist”

--- project-starter/app/api/coinDetail_routes.py
+++ project-starter/app/api/coinDetail_routes.py
@@ -14,7 +14,7 @@ def index(ticker):
     coinExist = Coin.query.filter(Coin.ticker.ilike(f"%{ticker}%")).first()
 
     def getHistory(ticker):
-      return cg.get_coin_market_chart_by_id(ticker, vs_currency='usd', days=1)
+        return cg.get_coin_market_chart_by_id(ticker, vs_currency='usd', days=1)
 
     try:
         inAPI = cg.get_coin_by_id(ticker)['name']
@@ -22,12 +22,12 @@ def index(ticker):
         return {'search': 'bad search'}
 
     if(not coinExist and inAPI):
-        print("------------ known coin loop")
+        # print("------------ known coin loop")
         history = getHistory(ticker)
         historic_prices = []
         for price in history['prices']:
             historic_prices.append({'price': price[1]})
-        print(historic_prices)
+        # print(historic_prices)
         newCoin = Coin()
         newCoin.ticker = ticker
         newCoin.name = cg.get_coin_by_id(ticker)['name']
@@ -39,7 +39,7 @@ def index(ticker):
     history = getHistory(ticker)
     for price in history['prices']:
         historic_prices.append({'price': price[1]})
-    print(historic_prices)
+    # print(historic_prices)
 
     data = cg.get_coin_by_id(ticker)
     return {'coin': data, 'prices': historic_prices}
