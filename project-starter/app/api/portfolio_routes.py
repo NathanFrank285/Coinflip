d9eeb386bbce58b7383dc6f930e2d9e80e506053
Nishi <naalnishinelson@gmail.com>
2021-04-28 17:08:36 -0700

    portfoliopage updates

--- project-starter/app/api/portfolio_routes.py
+++ project-starter/app/api/portfolio_routes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import db, Coin, Portfolio
+from pycoingecko import CoinGeckoAPI
 
+cg = CoinGeckoAPI()
 portfolio_routes = Blueprint('portfolio', __name__)
 
 @portfolio_routes.route('')
@@ -12,12 +14,15 @@ def getPortfolio():
       Portfolio.userId == userId).all()
   portfolio = {}
   for row in portfolioData:
-    # print(dir(row))
+    data = cg.get_price(ids=f'{row.coinInfo.ticker}', vs_currencies='usd', include_market_cap='true', include_24hr_vol='true', include_24hr_change='true', include_last_updated_at='true')
+    print(data, "here is the data!!!!!!!!!!!!!!")
+    # {'bitcoin': {'usd': 54697, 'usd_market_cap': 1022998512561.4462, 'usd_24h_vol': 48358885951.45435, 'usd_24h_change': -0.6118487157235886, 'last_updated_at': 1619652063}}
     portfolio[f'{row.coinInfo.ticker}'] = {
       'Name': row.coinInfo.name,
       'Quantity': row.quantity,
-      'Average Price': row.averagePrice,
+      'AveragePrice': row.averagePrice,
       'coinId': row.coinId,
+      'coinData': data[f"{row.coinInfo.ticker}"]
     }
   # print("------------",dir( portfolioData))
   # portfolio = [for row in portfolioData]
--- project-starter/react-app/src/components/PortfolioGraph/index.js
+++ project-starter/react-app/src/components/PortfolioGraph/index.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { NavLink } from 'react-router-dom'
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getPortfolioThunk } from "../../store/portfolio";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 import {
   LineChart,
   Line,
@@ -12,10 +21,125 @@ import {
   Legend,
 } from "recharts";
 import "./PortfolioGraph.css";
+
+
 export default function PortfolioGraph() {
+  const portfolio = useSelector(state => state?.portfolio?.Portfolio)
+
+
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(getPortfolioThunk());
   }, []);
-  return "this is my graph";
+
+  const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+        maxWidth: 1200,
+    },
+});
+
+const classes = useStyles()
+
+String.prototype.capitalize = function () {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
+function createData(name, price, dailychange, performance, volume) {
+    return { name, price, dailychange, performance, volume};
+}
+
+const capitalize = (s) => {
+  if (typeof s !== "string") return "";
+  return s.charAt(0).toUpperCase() + s.slice(1);
+};
+const formatCash = (n) => {
+  if (n < 1e3) return n;
+  if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + "K";
+  if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + "M";
+  if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + "B";
+  if (n >= 1e12) return +(n / 1e12).toFixed(2) + "T";
+};
+
+// const rows = portfolio?.map(coin => {
+//     const name = Object.keys(coin)
+    // return createData(
+    //   name,
+    //   coin[`${name[0]}`].usd,
+    //   coin[`${name[0]}`].usd_24h_change,
+    //   coin[`${name[0]}`].usd_24h_vol,
+    //   coin[`${name[0]}`].usd_market_cap
+    // );
+// })
+if(portfolio){
+  const tickers = Object.keys(portfolio)
+  console.log(tickers, "TTTTTTTTTTTTTTTTT")
+  for(let coin of tickers){
+    console.log(coin, "here is the coin!!")
+    return createData(
+      portfolio[`${coin}`]["Name"],
+      // portfolio.coin[`coinData`].usd,
+      // portfolio.coin[`coinData`].usd_24h_change,
+      // (portfolio.coin[`coinData`].usd),
+      // portfolio.coin[`coinData`].usd_24h_vol
+    );
+  }
+}
+
+let rows;
+// usd: 54816, usd_24h_change: 0.2844488253537837, usd_24h_vol: 46893571860.279526, usd_market_cap: 1024712665930.4342}
+
+return (
+<div className="portfolio-body">
+  <div className="table-container">
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell className="browser-head">Name</TableCell>
+            <TableCell className="browser-head" align="right">
+              Price
+            </TableCell>
+            <TableCell className="browser-head" align="right">
+              24hr Change
+            </TableCell>
+            <TableCell className="browser-head" align="right">
+              Performance
+            </TableCell>
+            <TableCell className="browser-head" align="right">
+              Volume
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows?.map((row) => (
+            <TableRow key={row?.name}>
+              <TableCell component="th" scope="row">
+                <NavLink
+                  className="browser-link"
+                  to={`/coinDetail/${row.name}`}
+                >
+                  {row.name}
+                </NavLink>
+              </TableCell>
+              <TableCell align="right" className="browser-data">
+                {"$" + row?.price.toFixed(2)}
+              </TableCell>
+              <TableCell align="right" className="browser-data">
+                {"%" + row?.change.toFixed(2)}
+              </TableCell>
+              <TableCell align="right" className="browser-data">
+                {formatCash(row?.volume)}
+              </TableCell>
+              <TableCell align="right" className="browser-data">
+                {formatCash(row?.marketCap)}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  </div>
+</div>
+);
 }
