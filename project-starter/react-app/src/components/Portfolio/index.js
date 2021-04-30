e2617ca5312c623ee6eb61d5a2461334360744da
Nishi <naalnishinelson@gmail.com>
2021-04-30 10:40:01 -0700

    styling port page

--- project-starter/react-app/src/components/Portfolio/index.js
+++ project-starter/react-app/src/components/Portfolio/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import WatchList from '../WatchList'
 import PortfolioGraph from '../PortfolioGraph'
+import './portfolio.css'
+
 const Portfolio = () => {
     return (
-        <div>
-            <div>
-            </div>
-            <div>
-                <PortfolioGraph />
-                <WatchList />
+        <div className="port-page">
+            <div className="port-container">
+                <div className="port-table">
+                    <PortfolioGraph />
+                </div>
+                <div className="watch-div">
+                    <WatchList />
+                </div>
             </div>
         </div>
     )
