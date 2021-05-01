28b6d383266930e22b8e0aba54c99b6fb3cba432
Nishi <naalnishinelson@gmail.com>
2021-04-30 17:20:27 -0700

    almost

--- project-starter/react-app/src/components/Portfolio/index.js
+++ project-starter/react-app/src/components/Portfolio/index.js
@@ -1,11 +1,30 @@
 import React from "react";
 import WatchList from '../WatchList'
 import PortfolioGraph from '../PortfolioGraph'
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPortfolioThunk } from "../../store/portfolio";
 import './portfolio.css'
 
 const Portfolio = () => {
+  const portfolio = useSelector((state) => state?.portfolio?.Portfolio);
+  const totalPortfolio = useSelector(state => state?.portfolio?.PortfolioTotalUsd);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPortfolioThunk());
+  }, []);
+
+  let total = 0
+
+  if(portfolio){
+      total = totalPortfolio.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+      })
+  }
     return (
         <div className="port-page">
+            <h1>Total Balance: {total}</h1>
             <div className="port-container">
                 <div className="port-table">
                     <PortfolioGraph />
