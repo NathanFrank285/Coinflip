cbb6099370d674a2fd1b3f7b4397d29b9a4f2a09
NathanFrank285 <nathansfrank@yahoo.com>
2021-06-07 10:43:42 -0700

    error handling for buy and sell buttons complete when trying to sell more quantity than you own or buy more than USD you have in your portfolio

--- project-starter/react-app/src/components/CoinDetail/index.js
+++ project-starter/react-app/src/components/CoinDetail/index.js
@@ -13,23 +13,23 @@ import { addToPortfolio, getPortfolioThunk, removeFromPortfolioThunk } from '../
 const CoinDetail = () => {
   const dispatch = useDispatch();
   const history = useHistory();
-  const { name } = useParams()
-  const details = useSelector(state => state?.coinDetail?.coin)
-  const portfolio = useSelector(state => state?.portfolio?.Portfolio)
+  const { name } = useParams();
+  const details = useSelector((state) => state?.coinDetail?.coin);
+  const portfolio = useSelector((state) => state?.portfolio?.Portfolio);
+  const portfolioUSD = useSelector(state => state?.portfolio?.PortfolioTotalUsd)
 
-  const inWatchlist = useSelector(state => state?.coinDetail?.inWatchlist)
-  const chartData24Hr = useSelector(state => state?.coinDetail?.prices24hr)
-  const chartData30 = useSelector(state => state?.coinDetail?.prices30)
-  const chartData300 = useSelector(state => state?.coinDetail?.prices300)
-  const chartData7days = useSelector(state => state?.coinDetail?.prices7days)
-  const badSearch = useSelector(state => state?.coinDetail?.search)
-  const [portfolioBuyClicked, setPortfolioBuyClicked] = useState('')
-  const [portfolioSellClicked, setPortfolioSellClicked] = useState('')
+  const inWatchlist = useSelector((state) => state?.coinDetail?.inWatchlist);
+  const chartData24Hr = useSelector((state) => state?.coinDetail?.prices24hr);
+  const chartData30 = useSelector((state) => state?.coinDetail?.prices30);
+  const chartData300 = useSelector((state) => state?.coinDetail?.prices300);
+  const chartData7days = useSelector((state) => state?.coinDetail?.prices7days);
+  const badSearch = useSelector((state) => state?.coinDetail?.search);
+  const [portfolioBuyClicked, setPortfolioBuyClicked] = useState("");
+  const [portfolioSellClicked, setPortfolioSellClicked] = useState("");
   const [watchlistStatus, setWatchlistStatus] = useState("");
-  const [graphStatus, setGraphStatus] = useState('');
-  const [quantity, setQuantity] = useState(0)
-  const [inPortfolio, setInPortfolio] = useState('')
-
+  const [graphStatus, setGraphStatus] = useState("");
+  const [quantity, setQuantity] = useState(0);
+  const [inPortfolio, setInPortfolio] = useState("");
 
   const formatCash = (n) => {
     if (n < 1e3) return n;
@@ -43,50 +43,49 @@ const CoinDetail = () => {
     dispatch(getCoinDetailThunk(name));
   }, [watchlistStatus]);
 
-
   if (badSearch === "bad search") {
-    history.go(0)
-    history.push('/portfolio')
+    history.go(0);
+    history.push("/portfolio");
   }
 
   const addTolist = () => {
     dispatch(addToWatchlist(name));
-    setWatchlistStatus(true)
-  }
+    setWatchlistStatus(true);
+  };
 
   const removeFromWatchlist = () => {
     dispatch(deleteFromWatchlist(name));
-    setWatchlistStatus(false)
+    setWatchlistStatus(false);
   };
   const graphStatusSetter = (time) => {
-    if (time === '24') {
-      setGraphStatus(chartData24Hr)
+    if (time === "24") {
+      setGraphStatus(chartData24Hr);
     }
-    if (time === '7') {
-      setGraphStatus(chartData7days)
+    if (time === "7") {
+      setGraphStatus(chartData7days);
     }
-    if (time === '30') {
-      setGraphStatus(chartData30)
+    if (time === "30") {
+      setGraphStatus(chartData30);
     }
-    if (time === '300') {
-      setGraphStatus(chartData300)
+    if (time === "300") {
+      setGraphStatus(chartData300);
     }
-  }
+  };
   const addToPortfolioSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const data = {
       coinId: details.id,
       quantity: quantity,
-      averagePrice: details?.market_data?.current_price?.usd
-    }
-    setInPortfolio(true)
-    setPortfolioBuyClicked(false)
-    dispatch(addToPortfolio(data))
-    history.push('/portfolio')
-  }
+      averagePrice: details?.market_data?.current_price?.usd,
+    };
+    setInPortfolio(true);
+    setPortfolioBuyClicked(false);
+    dispatch(addToPortfolio(data));
+    history.push("/portfolio");
+  };
   const removeFromPortfolio = (e) => {
     e.preventDefault();
-    console.log(details.id, quantity)
+    console.log(details.id, quantity);
     const data = {
       coinId: details.id,
       quantity: quantity,
@@ -96,7 +95,7 @@ const CoinDetail = () => {
     // }
     setPortfolioSellClicked(false);
     dispatch(removeFromPortfolioThunk(data));
-  }
+  };
   useEffect(() => {
     dispatch(getPortfolioThunk());
   }, [portfolio?.length]);
@@ -130,18 +129,24 @@ const CoinDetail = () => {
         )}
       </>
     );
+  } else {
+    portfolioButtonStuff = <div></div>;
   }
-  else {
-    portfolioButtonStuff = <div></div>
-  }
-
 
   let tradeForm;
-  if (portfolioBuyClicked){
+  if (portfolioBuyClicked) {
     tradeForm = (
       <div className="tradeForm">
         <form onSubmit={addToPortfolioSubmit}>
           <div className="portfolio-add-container">
+            {portfolioUSD <
+            quantity * details?.market_data?.current_price.usd ? (
+              <div className="sellError">
+                You do not have enough USD to make this purchase.
+              </div>
+            ) : (
+              <div></div>
+            )}
             <div>
               <label className="portfolio-add-label" for="quantity">
                 Quantity in coins
@@ -156,9 +161,20 @@ const CoinDetail = () => {
             ></input>
 
             <div className="buySellButtons">
-              <button type="submit" className="graph-buttons">
-                Buy
-              </button>
+              {portfolioUSD <
+              quantity * details?.market_data?.current_price.usd ? (
+                <button
+                  disabled={true}
+                  type="submit"
+                  className="graph-buttons disabled"
+                >
+                  Buy
+                </button>
+              ) : (
+                <button type="submit" className="graph-buttons">
+                  Buy
+                </button>
+              )}
               <button
                 onClick={() => setPortfolioBuyClicked(false)}
                 className="graph-buttons"
@@ -172,11 +188,20 @@ const CoinDetail = () => {
     );
   }
 
-  if (portfolioSellClicked){
+  if (portfolioSellClicked) {
     tradeForm = (
       <div className="tradeForm">
         <form onSubmit={removeFromPortfolio}>
           <div className="portfolio-add-container">
+            <div className="tradeError-container">
+              {quantity > portfolio[`${name}`].Quantity ? (
+                <div className="sellError">
+                  You cannot sell more coins than you own.
+                </div>
+              ) : (
+                <div></div>
+              )}
+            </div>
             <div>
               <label className="portfolio-add-label" for="quantity">
                 Quantity in coins
@@ -217,134 +242,124 @@ const CoinDetail = () => {
       </div>
     );
   }
-    return (
-      <div className="price-detail-container">
-        <div className="header">
-          <div className="header-info-container">
-            <div className="detail-coin-name">{details?.name}</div>
-            <div className="detail-coin-symbol">
-              {details?.symbol.toUpperCase()}
-            </div>
-          </div>
-          <div className="detail-coin-image-container">
-            <img className="detail-coin-image" src={details?.image.small} />
-          </div>
-          <div className="detail-button-container">
-            {inWatchlist == true && (
-              <button
-                onClick={removeFromWatchlist}
-                className="removeFromWatchList detail-watchlist-button"
-              >
-                Remove From Watch List
-              </button>
-            )}
-
-            {inWatchlist == false && (
-              <button
-                onClick={addTolist}
-                className="addToWatchList detail-watchlist-button"
-              >
-                Add to Watch List
-              </button>
-            )}
+  return (
+    <div className="price-detail-container">
+      <div className="header">
+        <div className="header-info-container">
+          <div className="detail-coin-name">{details?.name}</div>
+          <div className="detail-coin-symbol">
+            {details?.symbol.toUpperCase()}
           </div>
         </div>
-        <div className="graph-div">
-          <div className="detail-graph">
-            <ResponsiveContainer width="100%" height={400}>
-              <LineChart
-                data={graphStatus ? graphStatus : chartData24Hr}
-                margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
-              >
-                <YAxis domain={["auto", "auto"]} />
-                <XAxis
-                  type="category"
-                  dataKey="date"
-                  domain={["auto", "auto"]}
-                />
-                <Tooltip />
-
-                <Line
-                  type="linear"
-                  dot={false}
-                  dataKey="price"
-                  stroke="#8884d8"
-                />
-              </LineChart>
-            </ResponsiveContainer>
-          </div>
-          <div className="graph-buttons-container">
-            <button
-              className="graph-buttons"
-              onClick={() => graphStatusSetter("24")}
-            >
-              24Hr
-            </button>
-            <button
-              className="graph-buttons"
-              onClick={() => graphStatusSetter("7")}
-            >
-              7 Days
-            </button>
-            <button
-              className="graph-buttons"
-              onClick={() => graphStatusSetter("30")}
-            >
-              30 days
-            </button>
-
-          </div>
-
+        <div className="detail-coin-image-container">
+          <img className="detail-coin-image" src={details?.image.small} />
         </div>
-        <div className="market-detail">
-          {portfolioBuyClicked || portfolioSellClicked ? null : (
-            <div className="buySell-container">{portfolioButtonStuff}</div>
+        <div className="detail-button-container">
+          {inWatchlist == true && (
+            <button
+              onClick={removeFromWatchlist}
+              className="removeFromWatchList detail-watchlist-button"
+            >
+              Remove From Watch List
+            </button>
           )}
-          {portfolioBuyClicked ? tradeForm : null}
-          {portfolioSellClicked ? tradeForm : null}
-          <div className="market-detail-container">
-            <div>
-              Current Price:{" "}
-              {details?.market_data?.current_price.usd.toLocaleString("en-US", {
-                style: "currency",
-                currency: "USD",
-              })}
-            </div>
-            <div>
-              Total Supply:{" "}
-              {formatCash(details?.market_data?.circulating_supply)}
-            </div>
-            <div>
-              Market Cap : {formatCash(details?.market_data?.market_cap.usd)}
-            </div>
-            {details?.market_data?.price_change_percentage_24h >= 0 ? (
-              <div>
-                24Hr Change:{" "}
-                <span className="green">
-                  {details?.market_data?.price_change_percentage_24h.toFixed(2)}
-                  %
-                </span>
-              </div>
-            ) : (
-              <div>
-                24Hr Change:{" "}
-                <span className="red">
-                  {details?.market_data?.price_change_percentage_24h.toFixed(2)}
-                  %
-                </span>
-              </div>
-            )}
-            <div>
-              Total Volume: {formatCash(details?.market_data?.total_volume.usd)}
-            </div>
-          </div>
-        </div>
-        <div className="description">
-          {ReactHtmlParser(details?.description.de)}
+
+          {inWatchlist == false && (
+            <button
+              onClick={addTolist}
+              className="addToWatchList detail-watchlist-button"
+            >
+              Add to Watch List
+            </button>
+          )}
         </div>
       </div>
-    );
+      <div className="graph-div">
+        <div className="detail-graph">
+          <ResponsiveContainer width="100%" height={400}>
+            <LineChart
+              data={graphStatus ? graphStatus : chartData24Hr}
+              margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+            >
+              <YAxis domain={["auto", "auto"]} />
+              <XAxis type="category" dataKey="date" domain={["auto", "auto"]} />
+              <Tooltip />
 
+              <Line
+                type="linear"
+                dot={false}
+                dataKey="price"
+                stroke="#8884d8"
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="graph-buttons-container">
+          <button
+            className="graph-buttons"
+            onClick={() => graphStatusSetter("24")}
+          >
+            24Hr
+          </button>
+          <button
+            className="graph-buttons"
+            onClick={() => graphStatusSetter("7")}
+          >
+            7 Days
+          </button>
+          <button
+            className="graph-buttons"
+            onClick={() => graphStatusSetter("30")}
+          >
+            30 days
+          </button>
+        </div>
+      </div>
+      <div className="market-detail">
+        {portfolioBuyClicked || portfolioSellClicked ? null : (
+          <div className="buySell-container">{portfolioButtonStuff}</div>
+        )}
+        {portfolioBuyClicked ? tradeForm : null}
+        {portfolioSellClicked ? tradeForm : null}
+        <div className="market-detail-container">
+          <div>
+            Current Price:{" "}
+            {details?.market_data?.current_price.usd.toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+            })}
+          </div>
+          <div>
+            Total Supply: {formatCash(details?.market_data?.circulating_supply)}
+          </div>
+          <div>
+            Market Cap : {formatCash(details?.market_data?.market_cap.usd)}
+          </div>
+          {details?.market_data?.price_change_percentage_24h >= 0 ? (
+            <div>
+              24Hr Change:{" "}
+              <span className="green">
+                {details?.market_data?.price_change_percentage_24h.toFixed(2)}%
+              </span>
+            </div>
+          ) : (
+            <div>
+              24Hr Change:{" "}
+              <span className="red">
+                {details?.market_data?.price_change_percentage_24h.toFixed(2)}%
+              </span>
+            </div>
+          )}
+          <div>
+            Total Volume: {formatCash(details?.market_data?.total_volume.usd)}
+          </div>
+        </div>
+      </div>
+      <div className="description">
+        {ReactHtmlParser(details?.description.de)}
+      </div>
+    </div>
+  );
 }
 
 export default CoinDetail
